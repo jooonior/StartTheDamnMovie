78f99f7708cd4c4ce84331a34435f594e68f4298
junior <>
2022-09-09 12:48:15 -0800

    Remove unnecessary stuff from Plugin.h

--- Plugin.cpp
+++ Plugin.cpp
@@ -2,6 +2,11 @@
 
 #include "ServerPluginCallbacks.h"
 
+#define QUOTE(name) #name
+#define STR(macro) QUOTE(macro)
+
+#define PLUGIN_VERSION STR(PLUGIN_VERSION_MAJOR) "." STR(PLUGIN_VERSION_MINOR) "." STR(PLUGIN_VERSION_PATCH)
+
 class Plugin : public ServerPluginCallbacks
 {
 public:
@@ -20,7 +25,7 @@ Plugin::~Plugin() {}
 
 bool Plugin::Load(CreateInterfaceFn interfaceFactory, CreateInterfaceFn gameServerFactory)
 {
-	PluginMsg(PLUGIN_DESC " loaded.\n");
+	PluginMsg("Loading version %s\n", PLUGIN_VERSION);
 	return true;
 }
 
@@ -30,5 +35,5 @@ void Plugin::Unload(void)
 
 const char *Plugin::GetPluginDescription(void)
 {
-	return PLUGIN_DESC;
+	return PLUGIN_NAME " v" PLUGIN_VERSION;
 }
--- Plugin.h
+++ Plugin.h
@@ -1,19 +1,12 @@
 #pragma once
 
-#define CLIENT_DLL
-#include "cbase.h"
-
 #define PLUGIN_NAME "Plugin Template"
 #define PLUGIN_VERSION_MAJOR 1
 #define PLUGIN_VERSION_MINOR 0
 #define PLUGIN_VERSION_PATCH 0
 
-#define QUOTE(name) #name
-#define STR(macro) QUOTE(macro)
-
-#define PLUGIN_VERSION "v" STR(PLUGIN_VERSION_MAJOR) "." STR(PLUGIN_VERSION_MINOR) "." STR(PLUGIN_VERSION_PATCH)
-#define PLUGIN_DESC PLUGIN_NAME " " PLUGIN_VERSION
-
+#include <Color.h>
+#include <dbg.h>
 
 __forceinline void PluginPrintPrefix()
 {
